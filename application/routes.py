13cabf0029fbadc89c43972f4360e750c5548b86
Matt McMonagle <mmcmon1990@gmail.com>
2023-06-12 19:48:16 -0400

    Heroku set

--- application/routes.py
+++ application/routes.py
@@ -30,8 +30,8 @@ def bankclassify():
     input_data = json.dumps({"age": age, "job": job, "marital": marital, "education": education, "default": default, "balance": balance, "housing": housing, "loan": loan})
 
     #url for bank marketing model
-    url = "http://localhost:3000/api"
-    #url = "https://bank-model-app.herokuapp.com/api"
+    #url = "http://localhost:3000/api"
+    url = "https://bank-model-application.herokuapp.com/api"
   
     #post data to url
     results =  requests.post(url, input_data)
